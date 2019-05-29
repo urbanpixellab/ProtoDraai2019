cc7d09c4fccdf9eed35ac89d79ec9904d67c1e38
urbanpixellab <enrico@urbanpixellab.com>
2019-05-29 21:23:00 +0200

    implementation of microstopswitches, only workin in one direction

--- StepperMotorTest/StepperMotorTest.ino
+++ StepperMotorTest/StepperMotorTest.ino
@@ -1,10 +1,13 @@
 // defines pins numbers
+int maxSteps = 0;
 const int stepPin = 9; 
 const int dirPin = 8; 
-int stepDir = 0; //the direction of the stepper
+const int enablePin = 2;//enable 
+int stepDir = 1; //the direction of the stepper
 int microA,microB,dirBtnA,dirBtnB; //pin 4 tot 7
 int state = 0; //0 is calibration
 bool isCalibrated = false;
+bool leftEnable,rightEnable;
 //unsigned long maxStep = 10;
 //unsigned long thisStep = 0;
 // add schematic and thoughts
@@ -14,60 +17,63 @@ void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
-  pinMode(4,INPUT); //gruen
-  pinMode(5,INPUT); //blouw
-  pinMode(6,INPUT); //gruen
-  pinMode(7,INPUT); //blouw
-  state = 0;
+  pinMode(enablePin,OUTPUT);
+  pinMode(4,INPUT); //gruen //killswitch
+  pinMode(5,INPUT); //blouw //killswitch
+  pinMode(6,INPUT); //gruen //button
+  pinMode(7,INPUT); //blouw  //button
+  Serial.begin(9600);
+  digitalWrite(enablePin,HIGH);//disable
+  state = 1;
+  leftEnable= true;
+  rightEnable = true;
 }
 
 void loop() 
 {
   if(!isCalibrated) doCalibration();
-  
   //first read the button and switch inputs
   microA = digitalRead(4);
   microB = digitalRead(5);
   dirBtnA = digitalRead(6);
   dirBtnB = digitalRead(7);
 
+  Serial.print(microA);
+  Serial.print(":");
+  Serial.println(microB);
   switch (state) 
   {
     case 0:
       doCalibration();
       break;
     case 1:
-      // statements
-      if(dirBtnA == dirBtnB) return;
+      // statements use buttons to manualy drive stepper
+      if(dirBtnA == dirBtnB)
+      { 
+        digitalWrite(enablePin,HIGH);//disable
+        return;
+      }
       else
       {
-        if(dirBtnA == 0 && dirBtnB == 1)
+        if(dirBtnA != dirBtnB)
         {
-          if(stepDir != 0)
-          {
-            stepDir = 0;
-            digitalWrite(dirPin,stepDir);
-          }
-        }
-        else if(dirBtnA == 1 && dirBtnB == 0)
-        {
-          if(stepDir != 1)
-          {
-            stepDir = 1;
-            digitalWrite(dirPin,stepDir);
-          }
+          stepDir = 1;
+          if(dirBtnA == 1) stepDir = 0; 
+          digitalWrite(dirPin,stepDir);
+          doStep(100);
         }
       }
-      doStep();
       break;
+      
     default:
-      // statements
+
       break;
   }
 }
 
 void doCalibration()
 {
+  digitalWrite(enablePin,LOW);
   // calibration go backwards till microswitch, then one step back
   // then go foreward till microswitch and one step back, the counted steps are the maximum and this is equals to 1
   // then go to 0.5 as initial state which should be the half steps back
@@ -77,13 +83,41 @@ void doCalibration()
   state = 1;
 }
 
-void doStep()
+void doStep(int stepTime)
 {
-  for(int x = 0; x < 200; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(70); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(70); 
+//  maxSteps++;
+  digitalWrite(enablePin,LOW);//enable
+  delayMicroseconds(stepTime); 
+  for(int x = 0; x < 100; x++) {//200
+    //check killswitch
+      if(killSwitch(digitalRead(microA),digitalRead(microB))) return;
+      digitalWrite(stepPin,HIGH); 
+      delayMicroseconds(stepTime); 
+      digitalWrite(stepPin,LOW); 
+      delayMicroseconds(stepTime); 
   }
+//  Serial.println(maxSteps);}
+}
+
+bool killSwitch(bool low, bool high)
+{
+  if(low != high) 
+  {
+    leftEnable = low;
+    rightEnable= high;
+    if(stepDir == 0) digitalWrite(stepPin,HIGH);
+    else  digitalWrite(stepPin,LOW);
+    for(int i = 0;i < 2;i++)
+    {
+      digitalWrite(stepPin,HIGH); 
+      delayMicroseconds(100); 
+      digitalWrite(stepPin,LOW); 
+      delayMicroseconds(100); 
+      
+    }
+    digitalWrite(enablePin,HIGH);//disable
+    return true;
+  }
+  else return false;
 }
 
